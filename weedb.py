27bc95af6ad79aa7b07f0bb455ec70e1dc6492a2
ktf <not.discostoo@gmail.com>
2012-10-22 12:02:04 -0700

    oh hello

---
+++ weedb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+# Copyright 2012 Keith Fancher
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+
+
+def main():
+    print 'hey'
+
+
+if __name__ == '__main__':
+    main()
