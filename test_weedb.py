7f297f8058765bc01ed18f3a9a55d09bd2ba12ad
ktf <not.discostoo@gmail.com>
2012-10-22 16:36:30 -0700

    test first couple of given input/output pairs

--- test_weedb.py
+++ test_weedb.py
@@ -38,6 +38,8 @@ class TestCommands(unittest.TestCase):
         self.db._db['a'] = '10'
         self.db._get('a')
         self.assertEqual(self.output.getvalue(), "10\n")
+        self.db._get('asdf')
+        self.assertEqual(self.output.getvalue(), "10\nNULL\n")
 
     def test_unset(self):
         self.db._db['bleep'] = 'bloop'
@@ -48,6 +50,39 @@ class TestCommands(unittest.TestCase):
         self.db._db = {'a': '10', 'b': '10', 'c': '20', 'd': '10'}
         self.db._numequalto('10')
         self.assertEqual(self.output.getvalue(), "3\n")
+
+
+class TestThumbtackGivenInput(unittest.TestCase):
+
+    def setUp(self):
+        """Create a test instance and redirect stdout for testing."""
+        self.db = weedb.WeeDB()
+        self.output = StringIO.StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.output
+
+    def tearDown(self):
+        """Fix stdout now that we're through."""
+        self.output.close()
+        sys.stdout = self.saved_stdout
+
+    def test_sample_input_01(self):
+        self.db._set('a', '10')
+        self.db._get('a')
+        self.db._unset('a')
+        self.db._get('a')
+        self.assertEqual(self.output.getvalue(), "10\nNULL\n")
+
+    def test_sample_input_02(self):
+        self.db._set('a', '10')
+        self.db._set('b', '10')
+        self.db._numequalto('10')
+        self.db._numequalto('20')
+        self.db._unset('a')
+        self.db._numequalto('10')
+        self.db._set('b', '30')
+        self.db._numequalto('10')
+        self.assertEqual(self.output.getvalue(), "2\n0\n1\n0\n")
 
 
 if __name__ == '__main__':
