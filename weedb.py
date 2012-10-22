6c8c0c682e820d3ba4c5ba7b4676125a6069e8c9
ktf <not.discostoo@gmail.com>
2012-10-22 14:29:05 -0700

    basic command routing in place

--- weedb.py
+++ weedb.py
@@ -14,38 +14,89 @@ class InvalidCommandError(Exception):
     pass
 
 
+class InvalidArgumentError(Exception):
+    pass
+
+
 class WeeDB(object):
 
     def __init__(self):
         self._db = {}
-        self._commands = ['SET', 'GET', 'UNSET', 'NUMEQUALTO', 'END', 'BEGIN',
-                          'ROLLBACK', 'COMMIT']
+        self._commands = { 'SET': self._set,
+                           'GET': self._get,
+                           'UNSET': self._unset,
+                           'NUMEQUALTO': self._numequalto,
+                           'END': self._end,
+                           'BEGIN': self._begin,
+                           'ROLLBACK': self._rollback,
+                           'COMMIT': self._commit }
 
-    def parse_command(self, input_command):
-        """Parse a command string and return a tuple of (command, [args]),
-        where args is a list of arguments. If the command is invalid, raise an
-        InvalidCommandError expcetion."""
-        split = input_command.split()
-        command = split[0]
-        args = split[1:]
-
+    def execute_command(self, input_command):
+        """Parse and execute a database command, passed as a string. Can raise
+        either an InvalidCommandError or InvalidArgumentError if the input is
+        bad."""
+        command, args = self._parse_command(input_command)
         if not self._is_valid_command(command):
             raise InvalidCommandError("Sorry, WeeDB doesn't know that one.")
+        try:
+            self._commands[command](*args)
+        except TypeError:
+            raise InvalidArgumentError('Invalid argument(s)!')
+
+    def _parse_command(self, command):
+        """Parse a command string and return a tuple of (command, [args]),
+        where args is a list of arguments."""
+        com_list = command.split()
+        return (com_list[0].upper(), com_list[1:])
 
     def _is_valid_command(self, command):
         """Return True if the command is valid, False otherwise. """
-        if command.upper() in self._commands:
+        if command.upper() in self._commands.keys():
             return True
         else:
             return False
+
+    def _set(self, name, value):
+        print 'set ' + name + ' ' + value
+
+    def _get(self, name):
+        print 'get ' + name
+
+    def _unset(self, name):
+        print 'unset ' + name
+
+    def _numequalto(self, value):
+        print 'numequalto ' + value
+
+    def _end(self):
+        print 'end'
+
+    def _begin(self):
+        print 'begin'
+
+    def _rollback(self):
+        print 'rollback'
+
+    def _commit(self):
+        print 'commit'
 
 
 def main():
     db = WeeDB()
     try:
-        db.parse_command('this is not valid ok')
+        db.execute_command('SET a 10')
+        db.execute_command('GET a')
+        db.execute_command('UNSET a')
+        db.execute_command('NUMEQUALTO 10')
+        db.execute_command('BEGIN')
+        db.execute_command('ROLLBACK')
+        db.execute_command('COMMIT')
+        db.execute_command('END')
+        db.execute_command('BLARG')
     except InvalidCommandError:
         print 'Invalid command!'
+    except InvalidArgumentError:
+        print 'Invalid argument(s)!'
 
 
 if __name__ == '__main__':
